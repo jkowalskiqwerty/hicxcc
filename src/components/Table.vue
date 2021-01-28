eddfc013ca394004329a8c5cddfccb033472b6d2
jkowalskiqwerty <jkowalskiqwerty@gmail.com>
2021-01-28 21:24:57 +0100

    final

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,35 +1,90 @@
 <template>
-  <div class="about">
-    <h1>Table</h1>
-    <a-button type="primary" @click="mthOpenDrawer">Create</a-button>
+  <div>
+    <h1>Table Design</h1>
+
+    <div class="row">
+      <h2>Table Title</h2>
+      <a-button @click="mthOpenDrawer">Create</a-button>
+    </div>
+
     <a-drawer
       title="Data"
       placement="right"
       :visible="isDrawerOpened()"
-      :closable="true"
       @close="mthCloseTableDrawer()"
     >
+      <!-- :closable="true" -->
       <h1>Add/edit Table item</h1>
-      <form id="app" @submit="checkForm">
-        <label for="title">Title</label>
-        <input
-          name="title"
-          type="text"
-          v-model="title"
-          placeholder="please enter title"
-        />
-        <label for="status">Status</label>
-        <select placeholder="please choose status" name="status">
-          <option></option>
-          <option>draft</option>
-          <option>published</option>
-          <option>pending</option>
-        </select>
-        <br />
-        <br />
-        <a-button type="danger" @click="mthOpenDrawer" block>Create</a-button>
-      </form>
+      <label>Title</label>
+      <a-input
+        name="title"
+        type="text"
+        v-model="title"
+        placeholder="please enter title"
+      />
+      <label>Status</label>
+
+      <a-select
+        :value="status"
+        label-in-value
+        :default-value="{ key: '' }"
+        style="width: 100%; margin-left: 0px"
+        placeholder="choose status"
+        @change="mthStatusChanged"
+      >
+        <a-select-option value="draft">draft</a-select-option>
+        <a-select-option value="published">published</a-select-option>
+        <a-select-option value="pending">pending</a-select-option>
+      </a-select>
+
+      <!-- <select v-model="status" style="width: 100%; margin-left: 0px">
+        <option value=""></option>
+        <option value="draft">draft</option>
+        <option value="published">published</option>
+        <option value="pending">pending</option>
+      </select> -->
+
+      <div class="validation">{{ validation }}</div> 
+      <a-button type="danger" block @click="mthSave()">Save</a-button>
     </a-drawer>
+    <a-table :columns="columns" :data-source="itemsFromStore" rowKey="id">
+      <span slot="status" slot-scope="status">
+        <a-tag
+          :key="status.label"
+          :color="
+            status.label == 'draft'
+              ? 'volcano'
+              : status.label == 'published'
+              ? 'geekblue'
+              : 'green'
+          "
+        >
+          {{ status.label }}
+        </a-tag>
+      </span>
+
+      <a-pagination
+        :pageSize="1"
+        :defaultpageSize="1"
+        :total="1"
+      ></a-pagination>
+      <a slot="name" slot-scope="text">{{ text }}</a>
+      <span slot="customTitle"><a-icon type="smile-o" /> Name</span>
+
+      <span slot="action" slot-scope="text, record">
+        <a @click="mthEdit(record)">Edit</a>
+        |
+        <a @click="mthShowDeleteConfirmation(record)">Delete</a>
+      </span>
+    </a-table>
+
+    <a-modal
+      v-model="deleteConfirmationVisible"
+      title="Delete Confirmation"
+      @ok="mthDelete()"
+    >
+      <p>Are you sure you want to delete?</p>
+    </a-modal>
   </div>
 </template>
 
@@ -38,19 +93,131 @@ export default {
   name: "Table",
   data() {
     return {
+      id: 0,
       title: "",
-      status: "",
+      status: undefined,
+      validation: "",
+      deleteConfirmationVisible: false,
+      columns: [
+        {
+          title: "ID",
+          dataIndex: "id",
+          key: "id",
+          width: 50,
+        },
+        {
+          title: "Title",
+          dataIndex: "title",
+          key: "title",
+          width: 300,
+        },
+        {
+          title: "Status",
+          key: "status",
+          dataIndex: "status",
+          width: 150,
+          scopedSlots: { customRender: "status" },
+
+        },
+        {
+          title: "Actions",
+          key: "action",
+          scopedSlots: { customRender: "action" },
+          width: 150,
+        },
+      ],
     };
   },
+  computed: {
+    itemsFromStore() {
+      return this.$store.state.items;
+    },
+  },
   methods: {
+    mthStatusChanged(selectedItems) {
+      this.status = selectedItems;
+    },
+    isAlphanumeric() {
+      for (var i = 0; i < this.title.length; i++) {
+        var char1 = this.title.charAt(i);
+        var cc = char1.charCodeAt(0);
+
+        if (
+          (cc > 47 && cc < 58) ||
+          (cc > 64 && cc < 91) ||
+          (cc > 96 && cc < 123) || char1==' '
+        ) {
+        } else {
+          return false;
+        }
+      }
+      return true;
+    },
+    mthShowDeleteConfirmation(record) {
+      this.$store.dispatch("setRecordAction", record);
+      this.deleteConfirmationVisible = true;
+    },
+    mthDelete() {
+      this.$store.dispatch("deleteRecordAction");
+      this.deleteConfirmationVisible = false;
+      this.$notification.open({
+        message: "OK",
+        description: "Succesfully deleted.",
+        onClick: () => {},
+      });
+    },
+    mthEdit(record) {
+      this.id = record.id;
+      this.status = record.status;
+      this.title = record.title;
+      this.$store.dispatch("setEditRecordAction", record);
+
+    },
     mthOpenDrawer() {
+      this.id = 0;
+      this.title = "";
+      this.status = undefined;
+      this.validation="";
       this.$store.dispatch("openTableDrawerAction");
     },
     mthCloseTableDrawer() {
       this.$store.dispatch("closeTableDrawerAction");
     },
+    mthIsValid() {
+      this.validation = "";
+      if (this.title.length < 3 || this.title.length > 20) {
+        this.validation += "Title length should be between 3 and 20 chars. ";
+        return false;
+      } else if (this.status == "") {
+        this.validation += "Status is required.";
+        return false;
+      } else if (!this.isAlphanumeric(this.title)) {
+        this.validation += "Text can only consits of alphanumeric chars.";
+        return false;
+      }
+      return true;
+    },
+    mthSave() {
+      if (this.mthIsValid()) {
+        this.$store.dispatch("insertOrUpdateDataItemAction", {
+          id: this.id,
+          title: this.title,
+          status: this.status,
+        });
+
+        var message = "Successfully created.";
+        if (this.id != 0) message = "Successfully updated.";
+
+        this.$notification.open({
+          message: "OK",
+          description: message,
+          onClick: () => {},
+        });
+
+        this.validation = "";
+      }
+    },
     isDrawerOpened() {
-      //console.log(this.$store.state.tableDrawerOpened);
       return this.$store.state.tableDrawerOpened;
     },
   },
@@ -61,5 +228,22 @@ export default {
 label {
   display: block;
   margin-top: 20px;
+}
+div {
+  float: left;
+  margin: 20px;
+}
+.ant-select {
+  width: 100%;
+  margin-top: 0px;
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  float: initial;
+}
+.validation{
+  margin-top: 10px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    tableDrawerOpened: false
+    tableDrawerOpened: false,
+    items: [],
+    editingrecord: {},
   },
   mutations: {
     openTableDrawerMutation(state) {
-      console.log('openTableDrawer mutation fired');
-
-      state.tableDrawerOpened=true;
+      state.tableDrawerOpened = true;
     },
     closeTableDrawerMutation(state) {
-      state.tableDrawerOpened=false;
+      state.tableDrawerOpened = false;
+    },
+    deleteRecordMutation(state) {
+      //searching for an index by id to delete then removing from array
+      const indx = state.items.findIndex(v => v.id == state.editingrecord.id);
+      state.items.splice(indx, indx >= 0 ? 1 : 0);
+    },
+    insertOrUpdateTableDataItemMutation(state, item) {
+      //id=0 means insert otherwise delete
+      if(item.id==0)
+      {
+        var id=1;
+        if(state.items.length>0) id=Math.max.apply(Math, state.items.map(function(o) { return o.id; }))+1;
+        var itemtosave = { id: id, title: item.title, status: item.status };
+        state.items.push(itemtosave);
+      }
+      else
+      {
+        const indx = state.items.findIndex(v => v.id == item.id);
+        state.items[indx]=item;
+      }
+
+
     },
   },
   actions: {
@@ -23,6 +45,23 @@ export default new Vuex.Store({
     },
     closeTableDrawerAction({ commit }) {
       commit("closeTableDrawerMutation");
+    },
+    deleteRecordAction({ commit }) {
+      commit("deleteRecordMutation");
+    },
+    insertOrUpdateDataItemAction({ commit }, item) {
+      commit("insertOrUpdateTableDataItemMutation", item);
+      commit("closeTableDrawerMutation");
+
+    },
+    setEditRecordAction({ commit }, record) {
+      //set (bind) record and open drawer
+      this.state.editingrecord = record;
+      commit("openTableDrawerMutation");
+    },
+    setRecordAction({ commit }, record) {
+      //only set (bind) record to be deleted
+      this.state.editingrecord = record;
     },
   },
   modules: {
